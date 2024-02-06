2c3d20d59a6f55ad9242193dd992bfc30a268f2b
Pol <polmonteroalegre@gmail.com>
2024-02-06 12:32:48 +0100

    BIG COMMIT: functional snake game on lever pressed, README added

---
+++ README.md
@@ -0,0 +1,46 @@
+# Project Tv 📺
+
+¡Welcome all to Pixel Tv Project! This is the Week Two Project and second challenge at GeeksHubs Academy, consisting in a interactive tv with diferent channels, volume, and more.
+
+<img src="./img/gif_README.gif" alt="gif_page">
+
+## 🚀 GitHub pages prevew
+
+You can get a prevew of the webside on the actions tab in GitHub or directly through this [link](https://hypoldev.github.io/Menu_Week-1-Project/).
+
+## 🤖 How does it work? 
+
+Just click the start button and start enjoying
+
+## 🛠️ Tech&Tolls used 
+
+
+<img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub" /><img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" /><img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS" /><img src="https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=000&style=for-the-badge" alt="js"/>
+
+
+## 🏙️ Images and Resources
+
+
+- **Bing IA** - Responsible for the styling of the primitive canvas TV.
+
+Both of them open to public use in the Terms and Conditions of its parent companies.
+
+## ✒️ Autor
+
+- **Pol Montero** - Project Developer
+  - [GitHub](https://github.com/hypoldev) 
+
+## 🎓 Special Thanks
+
+- To **Geekshubs Academy** for the trust and encouragement to develop this first project (1/12 i suppose).
+
+## 📄 Add Ons - Bugs and Dreams
+
+Project finished on a polishing state, it is posible to find bugs, probably with the snake game:
+- JS needs to be refactorized, the design is fully functional but not optimized
+    - SnakeGame is always on and just z-indexed up or down
+    - Clear screan should have its own function
+- JS also may be splited in two different scripts
+- Pxs were used as size unit, due to my incompetence. Won't happen again
+<br>
+I'm no graphic designer to this date however I still belive the styling of this TV can be perfected with minimal changes, and I encourarge you to perform them.
--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ let filterLength = 0
 
 let isOn = false
 
+let snakeOn = false
+
 power.addEventListener("click", () => {
 
     isOn = !isOn
@@ -33,11 +35,16 @@ power.addEventListener("click", () => {
     }
     else {
         screen.classList.remove(screen.classList[screen.classList.length - 1])
+        document.getElementById("snakeLever").style.marginTop = ""
+        document.getElementById("snake").style.visibility = "hidden"
+        document.getElementById("snake").style.zIndex = "0"
+
         //reset volume
         for (let i = volumeLength; i > 0; i--) {
 
             document.getElementById(`volDis${i}`)
                 .style.backgroundImage = ""
+
 
         }
         //reset filters
@@ -50,6 +57,102 @@ power.addEventListener("click", () => {
 
 })
 
+/////////////////////////////////not entirely my///////////////////////////////////////////77
+let gameLoopId;
+
+function init() {
+    let canvas = document.getElementById("snake");
+    let context = canvas.getContext("2d");
+    let box = 15;
+    let snake = [];
+    snake[0] = {
+        x: 8 * box,
+        y: 8 * box
+    }
+    let direction = "right";
+    let food = {
+        x: Math.floor(Math.random() * 15 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 1) * box
+    }
+
+    function criarBG() {
+        context.fillStyle = "lightgreen";
+        context.fillRect(0, 0, 16 * box, 16 * box);
+    }
+
+    function criarCobrinha() {
+        for (i = 0; i < snake.length; i++) {
+            context.fillStyle = "green";
+            context.fillRect(snake[i].x, snake[i].y, box, box);
+        }
+    }
+
+    function drawFood() {
+        context.fillStyle = "red";
+        context.fillRect(food.x, food.y, box, box);
+    }
+
+    //quando um evento acontece, detecta e chama uma função
+    document.addEventListener('keydown', update);
+
+    function update(event) {
+        if (event.keyCode == 37 && direction != 'right') direction = 'left';
+        if (event.keyCode == 38 && direction != 'down') direction = 'up';
+        if (event.keyCode == 39 && direction != 'left') direction = 'right';
+        if (event.keyCode == 40 && direction != 'up') direction = 'down';
+    }
+
+    function iniciargame() {
+
+        if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+        if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+        if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+        if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+        for (i = 1; i < snake.length; i++) {
+            if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+                clearInterval(game);
+                alert('Game Over :(');
+            }
+        }
+
+        criarBG();
+        criarCobrinha();
+        drawFood();
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        if (direction == "right") snakeX += box;
+        if (direction == "left") snakeX -= box;
+        if (direction == "up") snakeY -= box;
+        if (direction == "down") snakeY += box;
+
+        if (snakeX != food.x || snakeY != food.y) {
+            snake.pop(); //pop tira o último elemento da lista
+        } else {
+            food.x = Math.floor(Math.random() * 15 + 1) * box;
+            food.y = Math.floor(Math.random() * 15 + 1) * box;
+        }
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+
+        snake.unshift(newHead);
+
+
+    }
+
+    const game = setInterval(iniciargame, 100);
+
+}
+
+document.getElementById("snake").style.visibility = "hidden"
+document.getElementById("snake").style.zIndex = "0"
+init()
+/////////////////////////////////////////////////////////////////////////////////////
 
 arrayButtons.map(
     items => {
@@ -130,6 +233,29 @@ arrayButtons.map(
                         }
 
                         break;
+
+                    case "s":
+                        snakeOn = !snakeOn
+
+                        if (snakeOn) {
+                            // remove whats on screen 
+
+                            document.getElementById("snakeLever").style.marginTop = "10px"
+                            screen.classList.remove(screen.classList[screen.classList.length - 1])
+                            document.getElementById("snake").style.visibility = "visible"
+                            document.getElementById("snake").style.zIndex = "1"
+
+                        }
+                        else {
+                            document.getElementById("snakeLever").style.marginTop = ""
+                            document.getElementById("snake").style.visibility = "hidden"
+                            document.getElementById("snake").style.zIndex = "0"
+
+                            screen.classList.add("Whitenoise")
+
+                        }
+                        break;
+
 
 
                 }
